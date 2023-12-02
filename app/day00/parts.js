afae90a6c6c9439595a264a7dcff49010f711139
Rodolfo De Nadai <rdenadai@gmail.com>
2023-12-02 01:46:56 -0300

    chore: Min infra for Javascript && OCaml

---
+++ app/day00/parts.js
@@ -0,0 +1,8 @@
+const readFile = require("../support/utils");
+
+const main = () => {
+  const lines = readFile("day01/input.txt").split("\n");
+  return "Not Implemented";
+};
+
+console.info(main());
